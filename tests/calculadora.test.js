35cd28084700c7d28fd0bc174d29c405011b9811
Erick Paluan <epaluan@outlook.com>
2025-09-01 13:33:34 -0300

    corrige rótulos e adiciona validação na função de soma

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,11 +1,16 @@
 const calculadora = require("../models/calculadora");
 
-test("Deve somar 2 + 2 e retornar 4", () => {
+test("somar 2 + 2 deveria retornar 4", () => {
   const resultado = calculadora.somar(2, 2);
   expect(resultado).toBe(4);
 });
 
-test("Deve somar 5 + 100 e retornar 105", () => {
+test("somar 5 + 100 deveria retornar 105", () => {
   const resultado = calculadora.somar(5, 100);
   expect(resultado).toBe(105);
+});
+
+test("somar 'banana' + 100 deveria retornar 'Erro", () => {
+  const resultado = calculadora.somar("banana", 100);
+  expect(resultado).toBe("Erro");
 });
